25b87364164dc90c82acbcec97de86af4ff0ee80
shala <shala@SHALA>
2026-07-10 13:26:20 +0400

    Restore ProductExporter solution and initial app setup
    Restored all core solution and project files, including the solution file, main project, and entry point. The application is configured as a .NET 10.0 console app with a basic "Hello, World!" output.

--- ProductExporter/ProductExporter.Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductExporter.Models
-{
-    public sealed class Category
-    {
-        public string Name { get; set; } = null!;
-        public bool IsDeleted { get; set; }
-        public ICollection<Product> Products { get; set; } = new List<Product>();
-    }
-}
--- ProductExporter/ProductExporter.Services/Interfaces/IDataReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace ProductExporter.Services.Interfaces
-{
-    public interface IDataTableReader
-    {
-        DataTable GetDataFromMySql();
-    }
-}
